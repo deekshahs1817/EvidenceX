d34c45444e428713748320fd8ac15d6c8bdff9b0
Deeksha <your-email@example.com>
2026-04-14 15:19:43 +0530

    Migrate to new GitHub account

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ✅ IMPORTANT for GitHub Pages
+  base: "/EvidenceX/",
+
   server: {
     proxy: {
       '/api/cctv': {
